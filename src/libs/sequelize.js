15a6341687a2ec0a91deaa8fa4ab2b8c552d4629
Derek Paul <derek.paul@brilliant360.net>
2023-11-10 09:10:14 -0500

    done connection

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -5,9 +5,16 @@ const { dbConfig } = require('../utils/config')
 const setupModels = require('../db/models')
 
 const URI = dbConfig.dbUrl
+console.log(URI)
 const sequelizeConnection = new Sequelize(URI, {
   dialect: 'mysql',
-  logging: true
+  logging: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+    }
+  }
 })
 
 setupModels(sequelizeConnection)
